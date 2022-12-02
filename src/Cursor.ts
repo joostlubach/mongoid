82680b012f77ff9a84c446b5c71470c9d316cf19
Joost Lubach <joostlubach@gmail.com>
2022-12-02 13:45:41 +0100

    Added Cursor.map method.

--- src/Cursor.ts
+++ src/Cursor.ts
@@ -15,25 +15,19 @@ export default class Cursor<M extends Model> {
     return await this.query.count()
   }
 
-  public forEach(iterator: (model: M) => void | Promise<void>): Promise<void> {
-    return new Promise((resolve, reject) => {
-      const next = async (error?: Error, document?: any) => {
-        if (error != null) {
-          reject(error)
-        } else if (document == null) {
-          resolve()
-        } else {
-          try {
-            const model = await this.Model.hydrate(document) as M
-            await iterator(model)
-            this.cursor.next(next)
-          } catch (error: any) {
-            reject(error)
-          }
-        }
-      }
-      this.cursor.next(next)
-    })
+  public async forEach(iterator: (model: M) => void | Promise<void>): Promise<void> {
+    const promises = await this.cursor.map(async document => {
+      const model = await this.Model.hydrate(document) as M
+      await iterator(model)
+    }).toArray()
+    return await Promise.all(promises).then(() => undefined)
+  }
+
+  public async map<U>(iterator: (model: M) => U | Promise<U>): Promise<Promise<U>[]> {
+    return await this.cursor.map(async document => {
+      const model = await this.Model.hydrate(document) as M
+      return await iterator(model)
+    }).toArray()
   }
 
   public hasNext(): Promise<boolean> {
