e4c0e8267964885cbc7eece5170ecf2f7a1612f1
Joost Lubach <joostlubach@gmail.com>
2023-07-17 15:08:31 +0200

    Added ./types export.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,4 @@
+export * from 'validator/types'
 export { default as polymorphicRef } from './polymorphicRef'
-export { default as ref } from './ref'
+export { default as ref, type Options as RefOptions } from './ref'
 export { default as virtual } from './virtual'
-
-export type { Options as RefOptions } from './ref'
