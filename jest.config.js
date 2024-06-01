15eafc32f1d07689cc03f625758a479372788602
Joost Lubach <joostlubach@gmail.com>
2024-06-01 21:19:28 +0200

    Support for integration test differentiation

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,6 @@ module.exports = {
       }
     ],
   },
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testMatch: process.env.INTEGRATION_TESTS ? ['**/*.integrationtest.ts'] : ['**/*.test.ts'],
   testEnvironment: 'node'
 }
